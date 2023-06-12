6d4a89ac0747326ad482efd1e540ca0810831075
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-06-12 05:22:42 +0000

    Mon, Jun 12, 2023, 1:22 AM -04:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -123,8 +123,7 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    // TODO
-    // !pairsofcandidates
+    if()
     return;
 }
 
