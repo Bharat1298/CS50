de7bd6dffd6cbaab2b5158ddd8295ea0311c9b11
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-06-18 18:49:49 +0000

    Sun, Jun 18, 2023, 2:49 PM -04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,7 +130,6 @@ bool vote(int voter, int rank, string name)
 {
     for(int i = 0; i < candidate_count; i++){
         if(strcmp(name, candidates[i].name) == 0){
-            candidates[i].votes++;
             preferences[voter][rank] = i;
             return true;
         }
@@ -141,7 +140,15 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    // TODO
+    for(int i = 0; i < candidate_count; i++){
+        if(!candidates[i].eliminated){
+            for(int j = 0; j < candidate_count; j++){
+                if(preferences[j][i] == candidates[i]){
+                    candidates[i].votes++;
+                }
+            }
+        }
+    }
     return;
 }
 
