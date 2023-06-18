e0971d92376e87dcd5cdeb1ffed2fa10ccbcd126
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-06-18 18:39:22 +0000

    Sun, Jun 18, 2023, 2:39 PM -04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,6 +130,7 @@ bool vote(int voter, int rank, string name)
 {
     for(int i = 0; i < candidate_count; i++){
         if(strcmp(name, candidates[i].name) == 0){
+            candidates[i].votes++;
             preferences[voter][rank] = i;
             return true;
         }
