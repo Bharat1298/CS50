b9c3bef17eac9b930bd4c99c7e80e9e11da33229
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-06-23 19:55:10 +0000

    Fri, Jun 23, 2023, 3:55 PM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -4,8 +4,8 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < width; i++){
-        for(int j = 0; j < height; j++){
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++){
             image[i][j].rgbtRed = round((image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen) / 3.0);
             image[i][j].rgbtGreen = image[i][j].rgbtRed;
             image[i][j].rgbtBlue = image[i][j].rgbtRed;
@@ -17,8 +17,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < width; i++){
-        for(int j = 0; j < height; j++){
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++){
             BYTE originalRed = image[i][j].rgbtRed;
             BYTE originalGreen = image[i][j].rgbtGreen;
             BYTE originalBlue = image[i][j].rgbtBlue;
