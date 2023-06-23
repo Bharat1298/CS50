214edd2be11ad621cddb749b16cb82ecd5a3d9aa
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-06-23 19:51:57 +0000

    Fri, Jun 23, 2023, 3:51 PM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -22,15 +22,15 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             BYTE originalRed = image[i][j].rgbtRed;
             BYTE originalGreen = image[i][j].rgbtGreen;
             BYTE originalBlue = image[i][j].rgbtBlue;
-            image[i][j].rgbtRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
+            image[i][j].rgbtRed = round((.393 * originalRed) + (.769 * originalGreen) + (.189 * originalBlue));
             if(image[i][j].rgbtRed > 255){
                 image[i][j].rgbtRed = 255;
             }
-            image[i][j].rgbtGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
+            image[i][j].rgbtGreen = round((.349 * originalRed) + (.686 * originalGreen) + (.168 * originalBlue));
             if(image[i][j].rgbtGreen > 255){
                 image[i][j].rgbtGreen = 255;
             }
-            image[i][j].rgbtBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
+            image[i][j].rgbtBlue = round((.272 * originalRed) + (.534 * originalGreen) + (.131 * originalBlue));
             if(image[i][j].rgbtBlue > 255){
                 image[i][j].rgbtBlue = 255;
             }
