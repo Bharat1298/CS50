b0906286cba9fb3b76a2f2e0d8a91bee8a78be4c
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-06-23 20:39:20 +0000

    Fri, Jun 23, 2023, 4:39 PM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -20,29 +20,37 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     BYTE originalRed;
     BYTE originalGreen;
     BYTE originalBlue;
+    int sepiaRed;
+    int sepiaGreen;
+    int sepiaBlue;
     for(int i = 0; i < width; i++){
         for(int j = 0; j < height; j++){
             originalRed = image[i][j].rgbtRed;
             originalGreen = image[i][j].rgbtGreen;
             originalBlue = image[i][j].rgbtBlue;
-            image[i][j].rgbtRed = round(.393 * originalRed
-                                      + .769 * originalGreen
-                                      + .189 * originalBlue);
-            // if(image[i][j].rgbtRed > 255){
-            //     image[i][j].rgbtRed = 255;
-            // }
-            image[i][j].rgbtGreen = round(.349 * originalRed
-                                        + .686 * originalGreen
-                                        + .168 * originalBlue);
-            // if(image[i][j].rgbtGreen > 255){
-            //     image[i][j].rgbtGreen = 255;
-            // }
-            image[i][j].rgbtBlue = round(.272 * originalRed
-                                       + .534 * originalGreen
-                                       + .131 * originalBlue);
-            // if(image[i][j].rgbtBlue > 255){
-            //     image[i][j].rgbtBlue = 255;
-            // }
+
+            sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
+            if(sepiaRed > 255){
+                image[i][j].rgbtRed = 255;
+            }else{
+                image[i][j].rgbtRed = sepiaRed;
+            }
+
+            sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
+            if(sepiaGreen > 255){
+                image[i][j].rgbtGreen = 255;
+            }
+            else{
+                image[i][j].rgbtGreen = sepiaGreen;
+            }
+
+            sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
+            if(sepiaBlue > 255){
+                image[i][j].rgbtBlue = 255;
+            }
+            else{
+                image[i][j].rgbtBlue = sepiaBlue;
+            }
         }
     }
     return;
