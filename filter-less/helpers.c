bc5d1ca56a148086d0f97665c8d15106a1340dd0
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-06-23 19:42:10 +0000

    Fri, Jun 23, 2023, 3:42 PM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -17,6 +17,25 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int i = 0; i < width; i++){
+        for(int j = 0; j < height; j++){
+            BYTE originalRed = image[i][j].rgbtRed;
+            BYTE originalGreen = image[i][j].rgbtGreen;
+            BYTE originalBlue = image[i][j].rgbtBlue;
+            image[i][j].rgbtRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
+            if(image[i][j].rgbtRed > 255){
+                image[i][j].rgbtRed = 255;
+            }
+            image[i][j].rgbtGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
+            if(image[i][j].rgbtBlue > 255){
+                image[i][j].rgbtBlue = 255;
+            }
+            image[i][j].rgbtBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
+            if(image[i][j].rgbtGreen > 255){
+                image[i][j].rgbtGreen = 255;
+            }
+        }
+    }
     return;
 }
 
