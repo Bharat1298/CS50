461607a0a511ff6da872dda9cd06d502d0722047
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-06-25 17:00:30 +0000

    Sun, Jun 25, 2023, 1:00 PM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -80,20 +80,92 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         }
     }
 
+    RGBTRIPLE topLeft, topMid, topRight, midLeft, mid, midRight, botLeft, botMid, botRight;
+
     for(int i = 0; i < height; i++){
         for(int j = 0; j < width; j++){
-            if(i != 0 && i != height - 1 && j != 0 && j != width - 1){
-                image[i][j].rgbtRed = round((temp[i - 1][j - 1].rgbtRed + temp[i - 1][j].rgbtRed + temp[i - 1][j + 1].rgbtRed
-                                    + temp[i][j - 1].rgbtRed + temp[i][j].rgbtRed + temp[i][j + 1].rgbtRed
-                                    + temp[i + 1][j - 1].rgbtRed + temp[i + 1][j].rgbtRed + temp[i + 1][j + 1].rgbtRed) / 9.0);
-                image[i][j].rgbtGreen = round((temp[i - 1][j - 1].rgbtGreen + temp[i - 1][j].rgbtGreen + temp[i - 1][j + 1].rgbtGreen
-                                    + temp[i][j - 1].rgbtGreen + temp[i][j].rgbtGreen + temp[i][j + 1].rgbtGreen
-                                    + temp[i + 1][j - 1].rgbtGreen + temp[i + 1][j].rgbtGreen + temp[i + 1][j + 1].rgbtGreen) / 9.0);
-                image[i][j].rgbtBlue = round((temp[i - 1][j - 1].rgbtBlue + temp[i - 1][j].rgbtBlue + temp[i - 1][j + 1].rgbtBlue
-                                    + temp[i][j - 1].rgbtBlue + temp[i][j].rgbtBlue + temp[i][j + 1].rgbtBlue
-                                    + temp[i + 1][j - 1].rgbtBlue + temp[i + 1][j].rgbtBlue + temp[i + 1][j + 1].rgbtBlue) / 9.0);
+            topLeft =   temp[i - 1][j - 1];
+            topMid =    temp[i - 1][j];
+            topRight =  temp[i - 1][j + 1];
+            midLeft =   temp[i][j - 1];
+            mid =       temp[i][j];
+            midRight =  temp[i][j + 1];
+            botLeft =   temp[i + 1][j - 1];
+            botMid =    temp[i + 1][j];
+            botRight =  temp[i + 1][j + 1];
+            if(i != 0 && i != height - 1 && j != 0 && j != width - 1){ //Middle Pixels
+                image[i][j].rgbtRed = round((topLeft.rgbtRed + topMid.rgbtRed + topRight.rgbtRed
+                                    + midLeft.rgbtRed + mid.rgbtRed + midRight.rgbtRed
+                                    + botLeft.rgbtRed + botMid.rgbtRed + botRight.rgbtRed) / 9.0);
+                image[i][j].rgbtGreen = round((topLeft.rgbtGreen + topMid.rgbtGreen + topRight.rgbtGreen
+                                    + midLeft.rgbtGreen + mid.rgbtGreen + midRight.rgbtGreen
+                                    + botLeft.rgbtGreen + botMid.rgbtGreen + botRight.rgbtGreen) / 9.0);
+                image[i][j].rgbtBlue = round((topLeft.rgbtBlue + topMid.rgbtBlue + topRight.rgbtBlue
+                                    + midLeft.rgbtBlue + mid.rgbtBlue + midRight.rgbtBlue
+                                    + botLeft.rgbtBlue + botMid.rgbtBlue + botRight.rgbtBlue) / 9.0);
+            }else if(i == 0 && (j != 0 && j != width - 1)){ //Top Middle Pixels
+                image[i][j].rgbtRed = round((midLeft.rgbtRed + mid.rgbtRed + midRight.rgbtRed
+                                    + botLeft.rgbtRed + botMid.rgbtRed + botRight.rgbtRed) / 6.0);
+                image[i][j].rgbtGreen = round((midLeft.rgbtGreen + mid.rgbtGreen + midRight.rgbtGreen
+                                    + botLeft.rgbtGreen + botMid.rgbtGreen + botRight.rgbtGreen) / 6.0);
+                image[i][j].rgbtBlue = round((midLeft.rgbtBlue + mid.rgbtBlue + midRight.rgbtBlue
+                                    + botLeft.rgbtBlue + botMid.rgbtBlue + botRight.rgbtBlue) / 6.0);
+            }else if(i == height - 1 && (j != 0 && j != width - 1)){ //Bottom Middle Pixels
+                image[i][j].rgbtRed = round((topLeft.rgbtRed + topMid.rgbtRed + topRight.rgbtRed
+                                    + midLeft.rgbtRed + mid.rgbtRed + midRight.rgbtRed) / 6.0);
+                image[i][j].rgbtGreen = round((topLeft.rgbtGreen + topMid.rgbtGreen + topRight.rgbtGreen
+                                    + midLeft.rgbtGreen + mid.rgbtGreen + midRight.rgbtGreen) / 6.0);
+                image[i][j].rgbtBlue = round((topLeft.rgbtBlue + topMid.rgbtBlue + topRight.rgbtBlue
+                                    + midLeft.rgbtBlue + mid.rgbtBlue + midRight.rgbtBlue) / 6.0);
+            }else if(j == 0 && (i != 0 && i != height - 1)){ //Left Middle Pixels
+                image[i][j].rgbtRed = round((topMid.rgbtRed + topRight.rgbtRed
+                                    + mid.rgbtRed + midRight.rgbtRed
+                                    + botMid.rgbtRed + botRight.rgbtRed) / 6.0);
+                image[i][j].rgbtGreen = round((topMid.rgbtGreen + topRight.rgbtGreen
+                                    + mid.rgbtGreen + midRight.rgbtGreen
+                                    + botMid.rgbtGreen + botRight.rgbtGreen) / 6.0);
+                image[i][j].rgbtBlue = round((topMid.rgbtBlue + topRight.rgbtBlue
+                                    + mid.rgbtBlue + midRight.rgbtBlue
+                                    + botMid.rgbtBlue + botRight.rgbtBlue) / 6.0);
+            }else if(j == width - 1 && (i != 0 && i != height - 1)){ //Right Middle Pixels
+                image[i][j].rgbtRed = round((topLeft.rgbtRed + topMid.rgbtRed
+                                    + midLeft.rgbtRed + mid.rgbtRed
+                                    + botLeft.rgbtRed + botMid.rgbtRed) / 6.0);
+                image[i][j].rgbtGreen = round((topLeft.rgbtGreen + topMid.rgbtGreen
+                                    + midLeft.rgbtGreen + mid.rgbtGreen
+                                    + botLeft.rgbtGreen + botMid.rgbtGreen) / 6.0);
+                image[i][j].rgbtBlue = round((topLeft.rgbtBlue + topMid.rgbtBlue
+                                    + midLeft.rgbtBlue + mid.rgbtBlue
+                                    + botLeft.rgbtBlue + botMid.rgbtBlue) / 6.0);
+            }else if(i == 0 && j == 0){ //Top Left Corner
+                image[i][j].rgbtRed = round((mid.rgbtRed + midRight.rgbtRed
+                                    + botMid.rgbtRed + botRight.rgbtRed) / 4.0);
+                image[i][j].rgbtGreen = round((mid.rgbtGreen + midRight.rgbtGreen
+                                    + botMid.rgbtGreen + botRight.rgbtGreen) / 4.0);
+                image[i][j].rgbtBlue = round((mid.rgbtBlue + midRight.rgbtBlue
+                                    + botMid.rgbtBlue + botRight.rgbtBlue) / 4.0);
+            }else if(i == 0 && j == width - 1){ //Top Right Corner
+                image[i][j].rgbtRed = round((midLeft.rgbtRed + mid.rgbtRed
+                                    + botLeft.rgbtRed + botMid.rgbtRed) / 4.0);
+                image[i][j].rgbtGreen = round((midLeft.rgbtGreen + mid.rgbtGreen
+                                    + botLeft.rgbtGreen + botMid.rgbtGreen) / 4.0);
+                image[i][j].rgbtBlue = round((midLeft.rgbtBlue + mid.rgbtBlue
+                                    + botLeft.rgbtBlue + botMid.rgbtBlue) / 4.0);
+            }else if(i == height - 1 && j == 0){ //Bottom Left Corner
+                image[i][j].rgbtRed = round((topMid.rgbtRed + topRight.rgbtRed
+                                    + mid.rgbtRed + midRight.rgbtRed) / 4.0);
+                image[i][j].rgbtGreen = round((topMid.rgbtGreen + topRight.rgbtGreen
+                                    + mid.rgbtGreen + midRight.rgbtGreen) / 4.0);
+                image[i][j].rgbtBlue = round((topMid.rgbtBlue + topRight.rgbtBlue
+                                    + mid.rgbtBlue + midRight.rgbtBlue) / 4.0);
+            }else{ //Bottom Right
+                image[i][j].rgbtRed = round((topLeft.rgbtRed + topMid.rgbtRed
+                                    + midLeft.rgbtRed + mid.rgbtRed) / 4.0);
+                image[i][j].rgbtGreen = round((topLeft.rgbtGreen + topMid.rgbtGreen
+                                    + midLeft.rgbtGreen + mid.rgbtGreen) / 4.0);
+                image[i][j].rgbtBlue = round((topLeft.rgbtBlue + topMid.rgbtBlue
+                                    + midLeft.rgbtBlue + mid.rgbtBlue) / 4.0);
             }
-
         }
     }
 
