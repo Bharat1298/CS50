adf9af84aeea8d1df5aaa55e63a607bd04816857
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-06-25 19:50:42 +0000

    Sun, Jun 25, 2023, 3:50 PM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int main(int argc, char *argv[])
 {
+    typedef uint8_t BYTE;
+    int BLOCK_SIZE = 512 * BYTE;
     if(argc != 2){
         printf("Usage: ./recover IMAGE");
         return 1;
