e5e17297b1d609927259ca82b598c19dbf7a3fcb
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-06-25 19:32:44 +0000

    Sun, Jun 25, 2023, 3:32 PM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    if(argc != 2){
+        printf("Usage: ./recover IMAGE");
+        return 1;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+
+    
 }
