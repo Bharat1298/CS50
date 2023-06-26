1b45d0f045824873be8820453bfde3ea49874619
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-06-26 01:52:42 +0000

    Sun, Jun 25, 2023, 9:52 PM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     typedef uint8_t BYTE;
     BYTE Buffer[512];
+    int BLOCK_SIZE = 512;
     if(argc != 2){
         printf("Usage: ./recover IMAGE");
         return 1;
@@ -17,5 +18,8 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    while(fread(Buffer, BLOCK_SIZE, 1, input)){
+        fwrite(Buffer, BLOCK_SIZE, 1, output)
+    }
 
 }
