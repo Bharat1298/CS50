cc9023cc56f67bd04a662ead60dc0efcdf0c7a80
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-07-17 20:21:15 +0000

    Mon, Jul 17, 2023, 4:21 PM -04:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,6 +1,8 @@
 from pyfiglet import Figlet
-from sys import argv
+import sys
 import random
+
+argv = sys.argv
 
 figlet = Figlet()
 
@@ -13,6 +15,13 @@ num = random.randint(0, len(list))
 f = Figlet(font = list[num])
 
 if len(argv) > 2 and argv[1] == '-f' or '--font':
-    input = argv[2]
+    if argv[2] in list:
+        f = Figlet(font = argv[2])
+    else:
+        print("Invalid usage")
+        sys.exit
+elif len(argv) > 2:
+    print("Invalid usage")
+    sys.exit
 
 print (f.renderText(input))
