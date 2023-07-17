81907f56614dd226cdffdd0313f17e0c313f9817
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-07-17 13:55:41 +0000

    Mon, Jul 17, 2023, 9:55 AM -04:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,12 +1,14 @@
 from pyfiglet import Figlet
+import random
 
 figlet = Figlet()
 
-input = input("Input: ")
+input = input("Input: ").strip()
 
+list = figlet.getFonts()
 
+num = random.randint(0, len(list))
 
-f = Figlet(font='slant')
-
+f = Figlet(font = list[num])
 
 print (f.renderText(input))
