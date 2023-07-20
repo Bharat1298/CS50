6fe68d034cbf55f1ac20f74f7085744e177d7d36
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-07-20 16:16:35 +0000

    Thu, Jul 20, 2023, 12:16 PM -04:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,91 +1,48 @@
-from cs50 import get_int
-import math
+from cs50 import get_string
 
 def main():
-    card = get_int("Card Number: ")
 
-    sum = checksum(card)
+     strCard = get_string("Card Number: ").strip()
 
-    count = 0
+     one = strCard[0]
 
-    digitCount = card
+     two = strCard[0] + strCard[1]
 
-    while digitCount > 0:
-         digitCount /= 10
-         count += 1
+     sOne = int(one)
 
+     sTwo = int(two)
 
-    if sum % 10 == 0:
-         if count == 13:
-              print("VISA")
-         elif count == 15 and ((card / 10000000000000 == 34) or (card / 10000000000000 == 37)):
-              print("AMEX")
-         elif count == 16:
-              if card / 1000000000000000 < 5:
-                   print("VISA")
-              elif (card / 100000000000000 < 56) and (card / 100000000000000 > 50):
-                   print("MASTERCARD")
-              else:
-                   print("INVALID")
-         else:
-              print("INVALID")
-    else:
-         print("INVALID")
+     valid = checksum(strCard)
 
+     digits = len(strCard)
 
-def checksum(card):
+     brand = ""
 
-     card1 = 0
-     card2 = 0
+     if(valid):
+          if digits == 13:
+               brand = "VISA"
+          elif digits == 15 and (two == "34" or two =="37"):
+               brand = "AMEX"
+          elif digits == 16:
+               if sOne < 5:
+                    brand = "VISA"
+               elif (sTwo < 56) and (sTwo > 50):
+                    brand = "MASTERCARD"
+     else:
+          brand = "INVALID"
+
+     print(brand)
+
+def checksum(strCard):
+     card = int(strCard)
+     valid = False
      holder = 10
      holder2 = 1
-     num1 = math.trunc(card / 10)
-     num2 = card
      sum1 = 0
      sum2 = 0
+     sum = 0
+     num1 = 0
+     num2 = 0
 
-     while num1 > 0:
-          remainder = num1 % 10
-          num1 /= 100
-
-          card1 += remainder * holder
-
-          holder *= 100
-
-     while num2 > 0:
-          remainder2 = num2 % 10
-          num2 /= 100
-
-          sum2 += remainder2
-
-          card2 += remainder2 * holder2
-
-          holder2 *= 100
-
-     remainders = []
-
-     i = 0
-
-     while num1 > 0:
-          remainders.append((num1 % 10) * 2)
-          num1 /= 10
-
-          if remainder[i] >= 10:
-
-               x = remainders[i]
-
-               sum1 += x % 10
-
-               x /= 10
-
-               sum1 += x % 10
-          else:
-               sum1 += remainder[i]
-
-          i += 1
-
-     return sum1 + sum2
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
      main()
