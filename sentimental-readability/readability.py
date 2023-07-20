afdbba02f57bd8a443d513e145dfc4bf45da5ad5
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-07-20 17:10:49 +0000

    Thu, Jul 20, 2023, 1:10 PM -04:00

---
+++ sentimental-readability/readability.py
@@ -0,0 +1,37 @@
+from cs50 import get_string
+
+def main():
+    text = get_string("Text: ")
+
+    letter = letters(text)
+
+    word = words(text)
+
+    sentence = sentences(text)
+
+    L = letter / word * 100
+
+    S = sentence / word * 100
+
+    grade = round(0.0588 * L - 0.296 * S - 15.8)
+
+    if grade > 16:
+        print("Grade 16+")
+    elif grade < 1:
+        print("Before Grade 1")
+    else:
+        print(f"Grade {grade}")
+
+
+def letters(text):
+    count = 0
+    for letter in text:
+        if letter.isalpha():
+            count += 1
+
+def words(text):
+
+def sentences(text):
+
+if __name__ == "__main__":
+    main()
