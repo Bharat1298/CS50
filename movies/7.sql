d34c3d603bc7d845b2c775a6a71451dab8cfa5ea
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-07-26 19:42:32 +0000

    Wed, Jul 26, 2023, 3:42 PM -04:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,1 @@
-SELECT rating, title FROM (SELECT * FROM movies JOIN ratings ON movies.id = ratings.movie_id) WHERE year = 2010 ORDER BY rating DESC;
+SELECT rating, title FROM (SELECT * FROM movies JOIN ratings ON movies.id = ratings.movie_id) WHERE year = 2010 ORDER BY rating DESC, title ASC;
