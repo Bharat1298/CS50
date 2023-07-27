eb63144982e9f3ef02b41f1f6656203c8c9a7d94
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-07-27 20:02:07 +0000

    Thu, Jul 27, 2023, 4:02 PM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -21,3 +21,18 @@ In the call, I heard the thief say that they were planning to take the earliest 
 tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket."
 
 SELECT * FROM phone_calls WHERE year = 2021 AND day = 28 AND month = 7 AND duration < 60;
+
+Select name FROM people WHERE phone_number in
+(SELECT receiver FROM phone_calls WHERE year = 2021 AND day = 28 AND month = 7 AND duration < 60)
+AND passport_number in (SELECT passport_number FROM passengers WHERE flight_id IN
+(SELECT id FROM flights WHERE year = 2021 AND day = 29 AND month = 7 ORDER BY hour LIMIT 1));
+
+Select name FROM people WHERE phone_number IN
+(SELECT caller FROM phone_calls WHERE year = 2021 AND day = 28 AND month = 7 AND duration < 60)
+AND passport_number IN (SELECT passport_number FROM passengers WHERE flight_id IN
+(SELECT id FROM flights WHERE year = 2021 AND day = 29 AND month = 7 ORDER BY hour LIMIT 1))
+AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND day = 28 AND month = 7 AND activity = 'exit')
+AND id IN(SELECT person_id FROM bank_accounts WHERE account_number in
+(SELECT account_number FROM atm_transactions WHERE year = 2021 AND day = 28 AND month = 7 AND transaction_type = 'withdraw' and atm_location = 'Leggett Street'));
+
+SELECT activity FROM bakery_security_logs WHERE year = 2021 AND day = 28 AND month = 7;
