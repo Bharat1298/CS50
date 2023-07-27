d85b5c8a80655fa5db552afe84a843ef961c216a
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-07-27 19:07:46 +0000

    Thu, Jul 27, 2023, 3:07 PM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -19,3 +19,5 @@ and saw the thief there withdrawing some money."
 "As the thief was leaving the bakery, they called someone who talked to them for less than a minute.
 In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville
 tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket."
+
+SELECT * FROM phone_calls WHERE year = 2021 AND day = 28 AND month = 7 AND duration < 60;
