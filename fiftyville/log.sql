2f3c777aff85e875a058fb1a48450edf66cf4801
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-07-27 15:52:29 +0000

    Thu, Jul 27, 2023, 11:52 AM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-
+SELECT * FROM crime_scene_reports
+WHERE year = 2021 AND day = 28 AND month = 7 AND street = 'Humphrey Street';
+-- Finds reports that happened during the day and location of the incident
