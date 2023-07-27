8b1a558dcda04ee7a6fa7e55f696fc1bb4d1b4b1
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-07-27 20:51:32 +0000

    Thu, Jul 27, 2023, 4:51 PM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,33 +6,26 @@ SELECT * FROM interviews
 WHERE year = 2021 AND day = 28 AND month = 7 AND transcript LIKE '%bakery%';
 -- Finds transcripts of the interviews where the crime is described
 
-
-| 161 | Ruth    | 2021 | 7     | 28  |
-"Sometime within ten minutes of the theft, I saw the thief get into a car in
-the bakery parking lot and drive away. If you have security footage from the bakery parking lot,
-you might want to look for cars that left the parking lot in that time frame."
-| 162 | Eugene  | 2021 | 7     | 28  |
-"I don't know the thief's name, but it was someone I recognized. Earlier this morning,
-before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street
-and saw the thief there withdrawing some money."
-| 163 | Raymond | 2021 | 7     | 28  |
-"As the thief was leaving the bakery, they called someone who talked to them for less than a minute.
-In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville
-tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket."
-
 SELECT * FROM phone_calls WHERE year = 2021 AND day = 28 AND month = 7 AND duration < 60;
+-- Lists all calls that happened during the day of the theft that were less than 60 seconds.
 
 Select name FROM people WHERE phone_number in
-(SELECT receiver FROM phone_calls WHERE year = 2021 AND day = 28 AND month = 7 AND duration < 60)
-AND passport_number in (SELECT passport_number FROM passengers WHERE flight_id IN
-(SELECT id FROM flights WHERE year = 2021 AND day = 29 AND month = 7 ORDER BY hour LIMIT 1));
+(SELECT receiver FROM phone_calls WHERE year = 2021 AND day = 28 AND month = 7 AND duration < 60 AND caller = (SELECT phone_number FROM people WHERE name = 'Bruce'));
+-- Gives accomplice by seeing who got a call during the theft from the theif
 
 Select name FROM people WHERE phone_number IN
-(SELECT caller FROM phone_calls WHERE year = 2021 AND day = 28 AND month = 7 AND duration < 60)
+    (SELECT caller FROM phone_calls WHERE year = 2021 AND day = 28 AND month = 7 AND duration < 60)
 AND passport_number IN (SELECT passport_number FROM passengers WHERE flight_id IN
-(SELECT id FROM flights WHERE year = 2021 AND day = 29 AND month = 7 AND destination_airport_id = 4 ORDER BY hour DESC LIMIT 1))
-AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND day = 28 AND month = 7 AND activity = 'exit')
-AND id IN(SELECT person_id FROM bank_accounts WHERE account_number in
-(SELECT account_number FROM atm_transactions WHERE year = 2021 AND day = 28 AND month = 7 AND transaction_type = 'withdraw' and atm_location = 'Leggett Street'));
+    (SELECT id FROM flights WHERE year = 2021 AND day = 29 AND month = 7 AND destination_airport_id = 4 ORDER BY hour, minute ASC LIMIT 1))
+AND license_plate IN
+    (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND day = 28 AND month = 7 AND activity = 'exit' AND hour = 10 AND (minute > 15 AND minute < 30))
+AND id IN
+    (SELECT person_id FROM bank_accounts WHERE account_number in
+        (SELECT account_number FROM atm_transactions WHERE year = 2021 AND day = 28 AND month = 7 AND transaction_type = 'withdraw' and atm_location = 'Leggett Street'));
+-- Uses call information along with passport numbers of everyone on the earliest flight
+-- along with the license plate of the car that left right after the theft along with atm withdrawls
+-- to find the culprit.
 
-SELECT activity FROM bakery_security_logs WHERE year = 2021 AND day = 28 AND month = 7;`
+
+SELECT city FROM airports WHERE id = 4;
+-- Gives city name
