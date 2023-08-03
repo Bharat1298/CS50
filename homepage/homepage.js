40b907995dc8b9c09ebfcb20e4c51df19f90ae52
Bharat1298 <64766365+Bharat1298@users.noreply.github.com>
2023-08-03 16:57:52 +0000

    Thu, Aug 3, 2023, 12:57 PM -04:00

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,10 +1,10 @@
-document.getElementById('contact').addEventListener("mouseenter", enter('contact'));
-document.getElementById('contact').addEventListener("mouseleave", exit('contact'));
+// document.getElementById('contact').addEventListener("mouseenter", enter('contact'));
+// document.getElementById('contact').addEventListener("mouseleave", exit('contact'));
 
-function enter(button){
-    document.getElementById(button).classList.add("active");
-}
+// function enter(button){
+//     document.getElementById(button).classList.add("active");
+// }
 
-function exit(button){
-    document.getElementById(button).classList.remove("active");
-}
+// function exit(button){
+//     document.getElementById(button).classList.remove("active");
+// }
